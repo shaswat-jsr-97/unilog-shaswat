8e50f92d90599eace5bfdd928ec2aea4d7c79cbe
Raghav <raghav.kanwal@unicommerce.com>
2023-02-15 13:24:10 +0530

    Main Layout

--- layouts/NavBar/Navbar.tsx
+++ layouts/NavBar/Navbar.tsx
@@ -1,10 +1,14 @@
-import { Text } from '@chakra-ui/react';
+import { Avatar, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
+import styles from './navbar.module.scss';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 export default function NavBar() {
     return (
-        <>
-            <Text>Main Nav Bar comes here.</Text>
-        </>
+        <Flex className={styles.NavBar} flexDir="row" justify={`space-between`} align="center" px={4}>
+            <GiHamburgerMenu />
+            <Text fontSize="sm">Main Nav Bar comes here.</Text>
+            <Avatar size="xs"></Avatar>
+        </Flex>
     )
 }
