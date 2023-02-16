643e3d3fa18cf2dd2c8c1ca0213ab3fca40c8a29
Raghav <raghav.kanwal@unicommerce.com>
2023-02-17 01:02:34 +0530

    Clean add NProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,31 @@ import { AppProps } from 'next/app'
 
 import MainLayout from '../layouts/MainLayout'
 import '../styles/globals.css'
+import "nprogress/nprogress.css";
+import NProgress from 'nprogress';
+import { useEffect } from 'react';
+import router from 'next/router';
+
+NProgress.configure({showSpinner:false})
 
 function MyApp({ Component, pageProps }: AppProps) {
+    
+    useEffect(() => {
+        const handleRouteStart = () => NProgress.start();
+        const handleRouteDone = () => NProgress.done();
+     
+        router.events.on("routeChangeStart", handleRouteStart);
+        router.events.on("routeChangeComplete", handleRouteDone);
+        router.events.on("routeChangeError", handleRouteDone);
+     
+        return () => {
+          // Make sure to remove the event handler on unmount!
+          router.events.off("routeChangeStart", handleRouteStart);
+          router.events.off("routeChangeComplete", handleRouteDone);
+          router.events.off("routeChangeError", handleRouteDone);
+        };
+      }, []);
+
     return (
         <ChakraProvider resetCSS={true}>
             <MainLayout>
