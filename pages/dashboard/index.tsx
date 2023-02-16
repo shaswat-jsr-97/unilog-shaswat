9d1648289e3bc16745b761162934d642609d2514
Raghav <raghav.kanwal@unicommerce.com>
2023-02-16 20:54:45 +0530

    Updated

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,24 +1,43 @@
 import { Card, CardBody, CardHeader, Heading, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
 import React from 'react'
-import styles from './dashboard.module.scss';
+
+import styles from './dashboard.module.scss'
 
 export default function DashboardPage() {
     return (
         <Card w={`100%`}>
-             <CardHeader pb={0}>
-                <Heading size='md' color="gray.900">Dashboard</Heading>
+            <CardHeader pb={0}>
+                <Heading size="md" color="gray.900">
+                    Dashboard
+                </Heading>
             </CardHeader>
             <CardBody pt={2}>
                 <Tabs>
                     <TabList>
-                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">Overview</Tab>
-                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">Orders</Tab>
-                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">Shipments</Tab>
-                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">NDR</Tab>
-                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">RTO</Tab>
-                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">Courier</Tab>
-                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">Delays</Tab>
-                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">Tracking</Tab>
+                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">
+                            Overview
+                        </Tab>
+                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">
+                            Orders
+                        </Tab>
+                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">
+                            Shipments
+                        </Tab>
+                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">
+                            NDR
+                        </Tab>
+                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">
+                            RTO
+                        </Tab>
+                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">
+                            Courier
+                        </Tab>
+                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">
+                            Delays
+                        </Tab>
+                        <Tab className={styles.dashboardTab} fontSize="sm" fontWeight="bold">
+                            Tracking
+                        </Tab>
                     </TabList>
 
                     <TabPanels className={styles.dashboardTabPanel}>
--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,19 +1,24 @@
-import { ChevronRightIcon } from '@chakra-ui/icons';
+import { ChevronRightIcon } from '@chakra-ui/icons'
 import { Card, CardBody, CardHeader, Divider, Flex, Heading, Icon, List, ListItem, Text } from '@chakra-ui/react'
+import Link from 'next/link'
 import React from 'react'
-import { GiOrganigram } from 'react-icons/gi';
-import { TbFileInvoice } from 'react-icons/tb';
-import { FaTruckPickup } from 'react-icons/fa';
-import { BsCash } from 'react-icons/bs';
-import Link from 'next/link';
-import styles from './settings.module.scss';
+import { BsCash } from 'react-icons/bs'
+import { FaTruckPickup } from 'react-icons/fa'
+import { GiOrganigram } from 'react-icons/gi'
+import { TbFileInvoice } from 'react-icons/tb'
+
+import styles from './settings.module.scss'
 
 export default function SettingsPage() {
     return (
         <Card w={`100%`} variant="outline">
             <CardHeader>
-                <Heading size='md' color="gray.900">Settings</Heading>
-                <Text as="p" fontSize="xs" color="gray.500" mt={2}>Overview your configurations across UniLog. Select a category to begin.</Text>
+                <Heading size="md" color="gray.900">
+                    Settings
+                </Heading>
+                <Text as="p" fontSize="xs" color="gray.500" mt={2}>
+                    Overview your configurations across UniLog. Select a category to begin.
+                </Text>
             </CardHeader>
             <Divider />
             <CardBody>
@@ -22,7 +27,9 @@ export default function SettingsPage() {
                         <CardHeader>
                             <Flex flexDir="row" gap={4} align="center">
                                 <Icon as={GiOrganigram} fontSize="lg" />
-                                <Text as="span" fontSize="sm" fontWeight="bold">Company</Text>
+                                <Text as="span" fontSize="sm" fontWeight="bold">
+                                    Company
+                                </Text>
                             </Flex>
                         </CardHeader>
                         <Divider />
@@ -30,27 +37,41 @@ export default function SettingsPage() {
                             <List>
                                 <ListItem>
                                     <Flex align="center" justifyContent="space-between" className={styles.link}>
-                                        <Link href="#" >
-                                            <Text as="span" fontSize="sm">Company Profile</Text>
+                                        <Link href="#">
+                                            <Text as="span" fontSize="sm">
+                                                Company Profile
+                                            </Text>
                                         </Link>
                                         <ChevronRightIcon fontSize="sm" />
                                     </Flex>
                                 </ListItem>
                                 <ListItem>
                                     <Flex align="center" justifyContent="space-between" mt={2} className={styles.link}>
-                                        <Link href="#"><Text as="span" fontSize="sm">KYC</Text></Link>
+                                        <Link href="#">
+                                            <Text as="span" fontSize="sm">
+                                                KYC
+                                            </Text>
+                                        </Link>
                                         <ChevronRightIcon fontSize="sm" />
                                     </Flex>
                                 </ListItem>
                                 <ListItem>
                                     <Flex align="center" justifyContent="space-between" mt={2} className={styles.link}>
-                                        <Link href="#"><Text as="span" fontSize="sm">KYC International</Text></Link>
+                                        <Link href="#">
+                                            <Text as="span" fontSize="sm">
+                                                KYC International
+                                            </Text>
+                                        </Link>
                                         <ChevronRightIcon fontSize="sm" />
                                     </Flex>
                                 </ListItem>
                                 <ListItem>
                                     <Flex align="center" justifyContent="space-between" mt={2} className={styles.link}>
-                                        <Link href="#"><Text as="span" fontSize="sm">Change Password</Text></Link>
+                                        <Link href="#">
+                                            <Text as="span" fontSize="sm">
+                                                Change Password
+                                            </Text>
+                                        </Link>
                                         <ChevronRightIcon fontSize="sm" />
                                     </Flex>
                                 </ListItem>
@@ -61,7 +82,9 @@ export default function SettingsPage() {
                         <CardHeader>
                             <Flex flexDir="row" gap={4} align="center">
                                 <Icon as={FaTruckPickup} fontSize="lg" />
-                                <Text as="span" fontSize="sm" fontWeight="bold">Pickup Address</Text>
+                                <Text as="span" fontSize="sm" fontWeight="bold">
+                                    Pickup Address
+                                </Text>
                             </Flex>
                         </CardHeader>
                         <Divider />
@@ -69,7 +92,11 @@ export default function SettingsPage() {
                             <List>
                                 <ListItem className={styles.link}>
                                     <Flex align="center" justifyContent="space-between">
-                                    <Link href="#"><Text as="span" fontSize="sm">Manage Pickup Address</Text></Link>
+                                        <Link href="#">
+                                            <Text as="span" fontSize="sm">
+                                                Manage Pickup Address
+                                            </Text>
+                                        </Link>
                                         <ChevronRightIcon fontSize="sm" />
                                     </Flex>
                                 </ListItem>
@@ -80,7 +107,9 @@ export default function SettingsPage() {
                         <CardHeader>
                             <Flex flexDir="row" gap={4} align="center">
                                 <Icon as={BsCash} fontSize="lg" />
-                                <Text as="span" fontSize="sm" fontWeight="bold">COD Payments</Text>
+                                <Text as="span" fontSize="sm" fontWeight="bold">
+                                    COD Payments
+                                </Text>
                             </Flex>
                         </CardHeader>
                         <Divider />
@@ -88,19 +117,25 @@ export default function SettingsPage() {
                             <List>
                                 <ListItem className={styles.link}>
                                     <Flex align="center" justifyContent="space-between">
-                                        <Text as="span" fontSize="sm">Bank Details</Text>
+                                        <Text as="span" fontSize="sm">
+                                            Bank Details
+                                        </Text>
                                         <ChevronRightIcon fontSize="sm" />
                                     </Flex>
                                 </ListItem>
                                 <ListItem className={styles.link}>
                                     <Flex align="center" justifyContent="space-between" mt={2}>
-                                        <Text as="span" fontSize="sm">Early COD</Text>
+                                        <Text as="span" fontSize="sm">
+                                            Early COD
+                                        </Text>
                                         <ChevronRightIcon fontSize="sm" />
                                     </Flex>
                                 </ListItem>
                                 <ListItem className={styles.link}>
                                     <Flex align="center" justifyContent="space-between" mt={2}>
-                                        <Text as="span" fontSize="sm">Postpaid</Text>
+                                        <Text as="span" fontSize="sm">
+                                            Postpaid
+                                        </Text>
                                         <ChevronRightIcon fontSize="sm" />
                                     </Flex>
                                 </ListItem>
@@ -111,7 +146,9 @@ export default function SettingsPage() {
                         <CardHeader>
                             <Flex flexDir="row" gap={4}>
                                 <Icon as={TbFileInvoice} fontSize="lg" />
-                                <Text as="span" fontSize="sm" fontWeight="bold">Billing</Text>
+                                <Text as="span" fontSize="sm" fontWeight="bold">
+                                    Billing
+                                </Text>
                             </Flex>
                         </CardHeader>
                         <Divider />
@@ -119,13 +156,17 @@ export default function SettingsPage() {
                             <List>
                                 <ListItem className={styles.link}>
                                     <Flex align="center" justifyContent="space-between">
-                                        <Text as="span" fontSize="sm">GSTIN Invoicing</Text>
+                                        <Text as="span" fontSize="sm">
+                                            GSTIN Invoicing
+                                        </Text>
                                         <ChevronRightIcon fontSize="sm" />
                                     </Flex>
                                 </ListItem>
                                 <ListItem className={styles.link}>
                                     <Flex align="center" justifyContent="space-between" mt={2}>
-                                        <Text as="span" fontSize="sm">Billing Address</Text>
+                                        <Text as="span" fontSize="sm">
+                                            Billing Address
+                                        </Text>
                                         <ChevronRightIcon fontSize="sm" />
                                     </Flex>
                                 </ListItem>
