c8857db2ce9d6978e54c3a5ab26037a7d27af483
Raghav <raghav.kanwal@unicommerce.com>
2023-02-17 01:03:10 +0530

    Updated app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,31 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
+import router from 'next/router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import { useEffect } from 'react'
 
 import MainLayout from '../layouts/MainLayout'
 import '../styles/globals.css'
-import "nprogress/nprogress.css";
-import NProgress from 'nprogress';
-import { useEffect } from 'react';
-import router from 'next/router';
 
-NProgress.configure({showSpinner:false})
+NProgress.configure({ showSpinner: false })
 
 function MyApp({ Component, pageProps }: AppProps) {
-    
     useEffect(() => {
-        const handleRouteStart = () => NProgress.start();
-        const handleRouteDone = () => NProgress.done();
-     
-        router.events.on("routeChangeStart", handleRouteStart);
-        router.events.on("routeChangeComplete", handleRouteDone);
-        router.events.on("routeChangeError", handleRouteDone);
-     
+        const handleRouteStart = () => NProgress.start()
+        const handleRouteDone = () => NProgress.done()
+
+        router.events.on('routeChangeStart', handleRouteStart)
+        router.events.on('routeChangeComplete', handleRouteDone)
+        router.events.on('routeChangeError', handleRouteDone)
+
         return () => {
-          // Make sure to remove the event handler on unmount!
-          router.events.off("routeChangeStart", handleRouteStart);
-          router.events.off("routeChangeComplete", handleRouteDone);
-          router.events.off("routeChangeError", handleRouteDone);
-        };
-      }, []);
+            // Make sure to remove the event handler on unmount!
+            router.events.off('routeChangeStart', handleRouteStart)
+            router.events.off('routeChangeComplete', handleRouteDone)
+            router.events.off('routeChangeError', handleRouteDone)
+        }
+    }, [])
 
     return (
         <ChakraProvider resetCSS={true}>
